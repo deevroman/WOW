839a7e24bcfa311a2c8e2b5bcad9aab6415eb8fc
deevroman <roman.deev06@gmail.com>
2020-04-29 07:32:23 +0300

    fix commit

--- src/utils/poliz.h
+++ src/utils/poliz.h
@@ -7,7 +7,7 @@
 
 class Element {
 public:
-    int countParams = 0; // TODO можно перенести
+    int countParams = 0; // TODO возможно можно перенести
     enum PrimeFuncs {
         OR, AND, NOT,
         XOR, OR_BIT,
@@ -17,25 +17,29 @@ public:
         UNAR_PLUS, UNAR_MINUS, UNAR_TILDA,
         POW, COPY,
         GET_VALUE, INDEX_VALUE,
-        DEF_FUNC, DEF_CLASS,
+        GET_VALUES_FROM_PREV_STACK, // для вызовы функции. После def func кладём этот элемент c intValue =кол-ву аргументов, а потом copy, clear_stack для каждого аргумента
+        DEF_FUNC, DEF_CLASS, // def func нужно передать аргументы
         CALL_FUNC, CREATE_CLASS,
         GET_FIELD, EVAL_METHOD,
         GET_VALUE_INT, GET_VALUE_DOUBLE, GET_VALUE_STR,
         GET_VALUE_NONE, GET_VALUE_BOOL,
         CLEAR_STACK,
-        CMP_EQUAL, CMP_NOT_EQUAL, CMP_LESS, CMP_MORE, CMP_MORE_EQUAL, CMP_LESS_EQUAL
+        CMP_EQUAL, CMP_NOT_EQUAL, CMP_LESS, CMP_MORE, CMP_MORE_EQUAL, CMP_LESS_EQUAL,
+        JUMP_TO_SCOPE, // в intValue храним индекс в массиве otherScopes
+        RETURN_VALUE
     } TYPE;
-    int isJump = 0; // 0 - нет, иначе позиция
+    int posJump = 0;
     int intValue = 0;
     double doubleValue = 0.0; // TODO можно переписать на объединение
-    std::string stringValue = ""; // TODO на указатели сюда же имя функции
+    std::string stringValue = ""; // сюда же имя функции
 };
 
-class Poliz;
 
 class Poliz {
 public:
-    std::map<std::string, Poliz> funcs; // TODO classes
+    std::map<std::string, Poliz *> funcs;
+    std::map<std::string, Poliz *> classes;
+    std::vector<Poliz *> otherScopes;
     std::vector<Element> operations;
 };
 
